6ce181f229f3b89e4b8615c5f9289bf0307f4649
Laura Amaro <lauraamaro1804@gmail.com>
2023-05-22 17:58:47 -0300

    finish airbnb static page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,20 @@
+require "open-uri"
+
 class FlatsController < ApplicationController
+  before_action :set_flats
+
+  def index
+    if params[:flat_name]
+      @flats = @flats.select {|flat| flat["name"] == params[:flat_name]}
+    end
+  end
+
+  def show
+    @flat = @flats.find {|flat| flat["id"] == params[:id].to_i }
+  end
+
+  def set_flats
+    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    @flats = JSON.parse(URI.open(url).read)
+  end
 end
--- test/system/flats_test.rb
+++ test/system/flats_test.rb
@@ -1,9 +1,16 @@
 require "application_system_test_case"
 
 class FlatsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit flats_url
-  #
-  #   assert_selector "h1", text: "Flats"
-  # end
+  test "visit flats page" do
+    visit "/"
+
+    assert_selector "h1", text: "Flats"
+    assert_selector "h2", text: "Trendy Apt in Buttes Montmartre"
+  end
+
+  test "visiting a specific flat page" do
+    visit "/flats/145"
+
+    assert_selector "h1", text: "Charm at the Steps of the Sacre Coeur/Montmartre"
+  end
 end
